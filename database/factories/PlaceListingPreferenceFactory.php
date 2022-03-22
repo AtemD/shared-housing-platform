332cb8142b01b4cda41e87e20f9fc138b2b70e11
AtemD <manday.daniel@gmail.com>
2022-03-22 15:40:15 +0300

    add place listing preference factory

---
+++ database/factories/PlaceListingPreferenceFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\References\PeriodType;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PlaceListingPreferenceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => function () {
+                return User::factory()->create()->id;
+            },
+            'min_rent_amount' => $this->faker->randomElement([1000, 2000, 3000, 4000, 5000, 6000]),
+            'max_rent_amount' => $this->faker->randomElement([6000, 8000, 10000, 14000, 15000]),
+            'rent_period'=> $this->faker->randomElement(array_keys(PeriodType::periodTypeToDaysList())),
+            'availability_date' => Carbon::parse($this->faker->dateTimeBetween('2022/01/01', '2022/12/31')),
+        ];
+    }
+}
